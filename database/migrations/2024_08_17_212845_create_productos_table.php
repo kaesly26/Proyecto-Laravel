fbbf210a39f4e5bed04527868b0c71c6d5d65af7
kaesly26 <k4t70246@gmail.com>
2024-08-17 16:42:07 -0500

    modificacion en el schema de productos

--- database/migrations/2024_08_17_212845_create_productos_table.php
+++ database/migrations/2024_08_17_212845_create_productos_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->decimal('precio', 8, 2);
             $table->integer('stock');
             $table->text('descripcion')->nullable();
-            $table->foreignId('id_categoria')->constrained('categorias')->onDelete('cascade');
+            $table->unsignedBigInteger('id_categoria');
+            $table->foreign('id_categoria')->references('id_categoria')->on('categorias')->onDelete('cascade');
             $table->timestamps();
         });
     }
